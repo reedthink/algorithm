16638e8d132ef7d629f6a981f31b768e9fbf8d52
jkjk <reedthink@qq.com>
2023-02-20 23:57:29 +0800

    迭代二分查找

--- 二分查找.cpp
+++ 二分查找.cpp
@@ -3,43 +3,69 @@
 #include <algorithm>
 using namespace std;
 const int maxn = 1000;
-//查询策略类似lower_bound，二分查找到第一个等于k的位置
-int LowerBinarySearch(int lb, int ub, vector<int> a, int k)
+// 查询策略类似lower_bound，二分查找到第一个等于k的位置, 如果没有，返回-1
+// 与lower_bound()不同，lower_bound 查找的是数组中第一个大于等于val的数。
+int LowerBinarySearch(int left, int right, vector<int> a, int k)
 {
-    //目前还不够健壮，没有处理数组中不含查询元素的情况
-    lb -= 1; //暂时不明白为啥要这样
-    while (ub > lb + 1)
+    int ret = -1;
+    int mid;
+    right -= 1;
+    while (left < right)
     {
-        int mid = (lb + ub) / 2;
-        if (a[mid] >= k)
+        mid = (left + right) / 2;
+        if (a[mid] < k)
         {
-            ub = mid;
+            // 往右边找
+            left = mid + 1; // 因为mid对应的元素已经探测过，不等于。直接往右边找
+            ret = left;
         }
         else
         {
-            lb = mid;
+            // 往左边找(区间往左侧缩小)
+            right = mid; // 因为mid对应的元素已经探测过，可能等于，所以不更改
+            ret = right;
         }
     }
-    return ub; //下标
+    // 打个补丁
+    if (a[ret] != k)
+    {
+        ret = -1;
+    }
+    return ret; //下标
 }
 //查询策略类似upper_bound，二分查找到最后一个等于k的位置
-int UpperBinarySearch(int lb, int ub, vector<int> a, int k)
+int UpperBinarySearch(int left, int right, vector<int> a, int k)
 {
-    //目前不够健壮,，没有处理数组中不含查询元素的情况
-    lb--;
-    while (ub > lb + 1)
+    // 实现思路：一直往右侧收缩区间
+    int ret = -1; //数组中不含查询元素的情况，返回-1
+    int mid;
+    int time = 8;
+    right-=1;
+    while (left < right && time)
     {
-        int mid = (lb + ub) / 2;
-        if (a[mid] <= k)
+        time--;
+        mid = (left + right) / 2;
+        // cout << left << ' ' << right << ' ' << mid<<endl;
+        if (a[mid] > k)
         {
-            lb = mid;
+            // 往左边找(区间往左侧缩小)
+            right = mid; // 因为mid对应的元素已经探测过，不可能等于，但是这个写法是找最后一个大于等于k的值
+            ret = right;
         }
         else
         {
-            ub = mid;
+            // 往右边找
+            left = mid+1; // 因为mid对应的元素已经探测过，可能等于，所以留在mid位置
+            ret = left;
         }
     }
-    return lb;
+    // cout << left << ' ' << right << ' ' << mid<<endl;
+    // 打个补丁
+    if (a[ret] != k)
+    {
+        ret = -1;
+    }
+    return ret; //下标
 }
 void pr(vector<int> a)
 {
@@ -57,8 +83,8 @@ int main()
     pr(v);
 
     int k = 5;
-    cout << LowerBinarySearch(0, v.size(), v, k) << endl;
-    cout << UpperBinarySearch(0, v.size(), v, k) << endl;
+    cout << "下标：" << LowerBinarySearch(0, v.size(), v, k) << endl; // 左闭右开
+    cout << "下标：" << UpperBinarySearch(0, v.size(), v, k) << endl;
 
     // auto i = lower_bound(v.begin(), v.end(), 1) - v.begin();
 
